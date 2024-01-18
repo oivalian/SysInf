79f0820d3b1a08fc6ecdf06b700e6099c97370bc
oivalian <109859213+oivalian@users.noreply.github.com>
2024-01-18 21:33:27 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sysinf",
+    version="1.0.0",
+    author="oivalian",
+    packages=find_packages(),
+    install_requires=[
+        "psutil",
+        "platform",
+        "py-cpuinfo",
+        "GPUtil",
+    ],
+)
